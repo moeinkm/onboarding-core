66eeebeb429a9eaab94f725571b14947e73ed2b5
moein <moein.kameli.mk@gmail.com>
2024-09-27 15:02:47 -0700

    UP/ remove extra import

--- onboarding/core/api/views.py
+++ onboarding/core/api/views.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import viewsets
 from core.models import File
 from .serializers import FileSerializer, FileDataSerializer
 
